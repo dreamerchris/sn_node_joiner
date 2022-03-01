57c6bf60e0bfccb699fb6c98202f8b10346890c8
dreamerchris <51272727+dreamerchris@users.noreply.github.com>
2022-03-01 13:09:25 +0200

    trying to fix it 2

--- multi_sn_node_joiner.sh
+++ multi_sn_node_joiner.sh
@@ -19,7 +19,7 @@ PUBLIC_IP=$(echo $(curl -s ifconfig.me))
 
 for CURRENT_NODE in {1..$NODE_NUM}
 do
-SAFE_PORT=(12000+$CURRENT_NODE)
+SAFE_PORT= 12000+${CURRENT_NODE}
 
 CURRENT_ROOT_DIR=~/.safe/node/local_node$CURRENT_NODE/
 CURRENT_LOG_DIR=~/.safe/node/local_node$CURRENT_NODE/
