64eae474d0a36e6f7a3193d4a691d931cbd791f9
dreamerchris <51272727+dreamerchris@users.noreply.github.com>
2022-03-01 02:27:19 +0200

    update, creates user, installs safe and node

--- multi_sn_node_joiner.sh
+++ multi_sn_node_joiner.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-curl -so- https://raw.githubusercontent.com/maidsafe/safe_network/main/resources/scripts/install.sh | bash
-hash -r
-if sudo chown -R $(whoami) /tmp/sn_node*|| echo "no worries about the previous error!";
-then
-safe node install && echo "node install success";
-fi
+NEWUSER=node15
+sudo useradd -m -p yXl5LjtaUeOcM -s /bin/bash $NEWUSER
+sudo usermod -a -G sudo $NEWUSER
+sudo -i -u $NEWUSER bash << EOF
+curl -so- https://raw.githubusercontent.com/maidsafe/safe_network/main/resources/scripts/install.sh | bash;
+EOF
+if test -f /tmp/sn_node*; then sudo chown -R $NEWUSER /tmp/sn_node*; fi
+su $NEWUSER -c 'safe node install'
