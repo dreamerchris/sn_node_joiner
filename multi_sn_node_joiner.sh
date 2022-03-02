0a06593c5095fbe83b44a07ede1e6df7b24ae080
dreamerchris <51272727+dreamerchris@users.noreply.github.com>
2022-03-02 19:12:34 +0200

    working fully and joins 20 nodes

--- multi_sn_node_joiner.sh
+++ multi_sn_node_joiner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SAFENET="dreamnet"
 CONFIG_URL="https://nx23255.your-storageshare.de/s/F7e2QaDLNC2z94z/download/dreamnet.config"
-NODE_NUM=9
+NODE_NUM=20
 USER=$(whoami)
 
 
@@ -19,35 +19,34 @@ PUBLIC_IP=$(echo $(curl -s ifconfig.me))
 
 for CURRENT_NODE in  $(seq $NODE_NUM)
 do
-SAFE_PORT=$((12000+${CURRENT_NODE}))
+SAFE_PORT=$((12000+$CURRENT_NODE))
 CURRENT_ROOT_DIR=~/.safe/node/local_node$CURRENT_NODE/
 CURRENT_LOG_DIR=~/.safe/node/local_node$CURRENT_NODE/
 mkdir $CURRENT_ROOT_DIR
 
 echo -n "#!/bin/bash
 RUST_LOG=safe_network=trace,qp2p=info \
-	~/.safe/node/sn_node \
-	--max-capacity $VAULT_SIZE \
-	--local-addr "$LOCAL_IP":$SAFE_PORT \
-	--public-addr "$PUBLIC_IP":$SAFE_PORT \
-	--skip-auto-port-forwarding \
-	--root_dir "$CURRENT_ROOT_DIR" \
-	--log-dir "$CURRENT_LOG_DIR" & disown"\
-| tee ~/.safe/node/start-node${CURRENT_NODE}.sh
+        ~/.safe/node/sn_node \
+        --local-addr '$LOCAL_IP':$SAFE_PORT \
+        --public-addr '$PUBLIC_IP':$SAFE_PORT \
+        --skip-auto-port-forwarding \
+        --root-dir '$CURRENT_ROOT_DIR' \
+        --log-dir '$CURRENT_LOG_DIR' & disown" \
+| tee ~/.safe/node/start-node$CURRENT_NODE.sh
 
-chmod u+x ~/.safe/node/start-node${CURRENT_NODE}.sh
-	
+chmod u+x ~/.safe/node/start-node$CURRENT_NODE.sh
+
 echo -n "[Unit]
-Description=Safe Local Node ${CURRENT_NODE}
+Description=Safe Local Node $CURRENT_NODE
 [Service]
 User=$USER
-ExecStart=/home/$USER/.safe/node/start-node${CURRENT_NODE}.sh
+ExecStart=/home/$USER/.safe/node/start-node$CURRENT_NODE.sh
 RemainAfterExit=yes
 [Install]
 WantedBy=multi-user.target"\
-|sudo tee /etc/systemd/system/sn_node${CURRENT_NODE}.service
+| sudo tee /etc/systemd/system/sn_node$CURRENT_NODE.service
 
-sudo systemctl start sn_node${CURRENT_NODE}.service
+sudo systemctl start sn_node$CURRENT_NODE.service
 
 done
 
