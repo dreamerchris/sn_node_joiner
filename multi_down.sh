10083a73adc1550f2952a3ef3aa59f79646c7cd6
dreamerchris <51272727+dreamerchris@users.noreply.github.com>
2022-03-02 19:13:10 +0200

    working cleaning

--- multi_down.sh
+++ multi_down.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 safe node killall
-NODE_NUM=10
-for CURRENT_NODE in {1..$NODE_NUM}
+NODE_NUM=40
+for CURRENT_NODE in  $(seq $NODE_NUM)
 do
-sudo systemctl stop sn_node$CURRENT_NODE.service
+sudo systemctl stop sn_node${CURRENT_NODE}.service
 done
