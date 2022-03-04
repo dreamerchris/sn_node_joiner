d063e65f2c8f9d470b7f08b41dcd074197d0e00a
dreamerchris <51272727+dreamerchris@users.noreply.github.com>
2022-03-04 23:01:22 +0200

    added support for manjaro

---
+++ dep_installer_manjaro.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "apt update"
+sudo pacman -Syy >/dev/null && echo "pacman -Syy success!" || echo "ERROR pacman -Syy failed"
+
+echo "installing snapd build-essential moreutils"
+sudo pacman -S --noconfirm snapd base-devel moreutils >/dev/null && echo "snapd base-devel moreutils installed!" || echo "ERROR some or all failed to install! (snapd base-devel moreutils)"
+
+echo "installing curl through snap"
+sudo snap install curl && echo "curl installed!" || echo "ERROR curl failed to install!"
+
+echo "installing rust through snap"
+sudo snap install rustup --classic && echo "rust first install command success!" || echo "ERROR first rust install command failed!"
+
+echo "continue to installing rust"
+rustup toolchain install stable && echo "rust second install command success!" || echo "ERROR second rust install command failed!"
+
+echo "installing vdash"
+cargo install vdash && echo "vdash installed!" || echo "ERROR vdash failed to install"
+
+rm -rf "$HOME"/.safe && echo "succes in removing ~/.safe folder" # clear out any old files
+
+#get the CLI
+curl -so- https://raw.githubusercontent.com/maidsafe/safe_network/master/resources/scripts/install.sh | bash
+echo ""
+echo ""
+echo ""
+echo $(safe --version) "install complete"
+
+hash -r
+
+safe node install
+echo ""
+echo ""
+echo ""
+echo $(safe node bin-version) "install complete"
+
+echo "all deps installed! go run sh multi_sn_node_joiner.sh"
