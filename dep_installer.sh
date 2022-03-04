8da68b817fa9cff2ff2428198abe75341156a43b
dreamerchris <51272727+dreamerchris@users.noreply.github.com>
2022-03-04 21:21:38 +0200

    ready

--- dep_installer.sh
+++ dep_installer.sh
@@ -5,3 +5,22 @@ sudo snap install curl
 sudo snap install rustup --classic
 rustup toolchain install stable
 cargo install vdash
+
+rm -rf "$HOME"/.safe # clear out any old files
+
+#get the CLI
+curl -so- https://raw.githubusercontent.com/maidsafe/safe_network/master/resources/scripts/install.sh | bash
+echo ""
+echo ""
+echo ""
+echo $(safe --version) "install complete"
+
+hash -r
+
+safe node install
+echo ""
+echo ""
+echo ""
+echo $(safe node bin-version) "install complete"
+
+echo "all deps installed! go run sh multi_sn_node_joiner.sh"
