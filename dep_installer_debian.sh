eb3a9cf6ba7c6e937f887e1904399ced86c17dcd
dreamerchris <51272727+dreamerchris@users.noreply.github.com>
2022-03-05 13:04:01 +0200

    Update dep_installer_debian.sh

--- dep_installer_debian.sh
+++ dep_installer_debian.sh
@@ -3,8 +3,8 @@
 echo "apt update"
 sudo apt -qq update >/dev/null && echo "apt update success!" || echo "ERROR apt update failed"
 
-echo "installing snapd build-essential moreutils"
-sudo apt -qq install -y snapd build-essential moreutils tree >/dev/null && echo "snapd build-essential moreutils tree installed!" || echo "ERROR some or all failed to install! (snapd build-essential moreutils tree)"
+echo "installing snapd build-essential moreutils tree unzip"
+sudo apt -qq install -y snapd build-essential moreutils tree unzip >/dev/null && echo "snapd build-essential moreutils tree unzip installed!" || echo "ERROR some or all failed to install! (snapd build-essential moreutils tree unzip)"
 
 echo "installing curl through snap"
 sudo snap install curl && echo "curl installed!" || echo "ERROR curl failed to install!"
