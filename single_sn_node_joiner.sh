8834b1900c12a63e691e6d8961c812c6201faa23
dreamerchris <51272727+dreamerchris@users.noreply.github.com>
2022-03-05 12:25:28 +0200

    Create single_sn_node_joiner.sh

---
+++ single_sn_node_joiner.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+SAFENET="dreamnet"
+CONFIG_URL="https://nx23255.your-storageshare.de/s/F7e2QaDLNC2z94z/download/dreamnet.config"
+USER=$(whoami)
+
+safe networks add $SAFENET "$CONFIG_URL"
+safe networks switch $SAFENET
+
+ACTIVE_IF=$( ( cd /sys/class/net || exit; echo *)|awk '{print $1;}')
+LOCAL_IP=$(echo $(ifdata -pa "$ACTIVE_IF"))
+PUBLIC_IP=$(echo $(curl -s ifconfig.me))
+
+CURRENT_NODE=1
+SAFE_PORT=12001
+CURRENT_ROOT_DIR=$HOME/.safe/node/local_node$CURRENT_NODE/
+CURRENT_LOG_DIR=$HOME/.safe/node/local_node$CURRENT_NODE/
+mkdir $CURRENT_ROOT_DIR
+
+echo -n "#!/bin/bash
+RUST_LOG=safe_network=trace,qp2p=info \
+        $HOME/.safe/node/sn_node \
+        --local-addr '$LOCAL_IP':$SAFE_PORT \
+        --public-addr '$PUBLIC_IP':$SAFE_PORT \
+        --skip-auto-port-forwarding \
+        --root-dir '$CURRENT_ROOT_DIR' \
+        --log-dir '$CURRENT_LOG_DIR' & disown" \
+| tee $HOME/.safe/node/start-node$CURRENT_NODE.sh
+
+chmod u+x $HOME/.safe/node/start-node$CURRENT_NODE.sh
+
+echo -n "[Unit]
+Description=Safe Local Node $CURRENT_NODE
+[Service]
+User=$USER
+ExecStart=$HOME/.safe/node/start-node$CURRENT_NODE.sh
+Type=forking
+[Install]
+WantedBy=multi-user.target"\
+| sudo tee /etc/systemd/system/sn_node$CURRENT_NODE.service
+
+sudo systemctl start sn_node$CURRENT_NODE.service
+
+
+echo ""
+echo "End of multi sn node joiner script. Starting vdash!"
+echo ""
+
+$HOME/.cargo/bin/vdash $HOME/.safe/node/local_node*/sn_node.log
