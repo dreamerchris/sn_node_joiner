1a447deacabc1ec85d171bd00980b6c196464687
dreamerchris <51272727+dreamerchris@users.noreply.github.com>
2022-03-05 13:15:15 +0200

    Update single_down.sh

--- single_down.sh
+++ single_down.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+nohup safe node killall >/dev/null 2>&1
 
+CURRENT_NODE=1
+
+sudo systemctl stop sn_node${CURRENT_NODE}.service
+sudo systemctl disable sn_node${CURRENT_NODE}.service
+
+sudo systemctl daemon-reload
+sudo systemctl reset-failed
