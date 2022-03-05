71c72ec1fa274d9ab4ede1d3511b34191049495d
dreamerchris <51272727+dreamerchris@users.noreply.github.com>
2022-03-05 13:57:15 +0200

    Update playground_mutli_join_with_built_sn_node.sh

--- playground_mutli_join_with_built_sn_node.sh
+++ playground_mutli_join_with_built_sn_node.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 SAFENET="playground"
 CONFIG_URL="https://safe-testnet-tool.s3.eu-west-2.amazonaws.com/public-node_connection_info.config"
-
+safe networks add $SAFENET "$CONFIG_URL"
+safe networks switch $SAFENET
 safe cat safe://hyfenryyban3rya95df67upomjse4wjaumy1awb94hs8oyfezjnpwykunu6zo > sn_node.zip
 
 unzip sn_node.zip -d .
@@ -13,8 +14,7 @@ sudo cp sn_node $HOME/.safe/node/sn_node
 NODE_NUM=20
 USER=$(whoami)
 
-safe networks add $SAFENET "$CONFIG_URL"
-safe networks switch $SAFENET
+
 
 ACTIVE_IF=$( ( cd /sys/class/net || exit; echo *)|awk '{print $1;}')
 LOCAL_IP=$(echo $(ifdata -pa "$ACTIVE_IF"))
