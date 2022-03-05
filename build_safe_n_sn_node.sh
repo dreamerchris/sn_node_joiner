12e884aa3408b7c7c30f169b034e8a44cfa2af3c
dreamerchris <51272727+dreamerchris@users.noreply.github.com>
2022-03-05 12:22:59 +0200

    Update and rename single_sn_node_builder_n_joiner.sh to build_safe_n_sn_node.sh

---
+++ build_safe_n_sn_node.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+mkdir -p \
+	$HOME/github-tmp \
+	$HOME/.safe/cli \
+	$HOME/.safe/node
+
+git clone https://github.com/maidsafe/safe_network.git $HOME/github-tmp/
+cd $HOME/github-tmp
+cargo build --release
+cp $HOME/github-tmp/target/release/safe $HOME/.safe/cli/
+cp $HOME/github-tmp/target/release/sn_node $HOME/.safe/node/
+tree  $HOME/.safe
+
+PATH=$PATH:/$HOME/.safe/cli
+
+echo "now launch single_sn_node_joiner or multi_sn_node_joiner
